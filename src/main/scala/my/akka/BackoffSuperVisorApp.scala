a9eedf8583f040badda693cdf3a97c4aa6407931
Richard Imaoka <richard.e.imaoka@gmail.com>
2017-09-18 02:30:29 +0900

    onStop and onFailure comparison

--- src/main/scala/my/akka/BackoffSuperVisorApp.scala
+++ src/main/scala/my/akka/BackoffSuperVisorApp.scala
@@ -2,126 +2,256 @@ package my.akka
 
 import akka.actor.{Actor, ActorRef, ActorSystem, PoisonPill, Props}
 import akka.pattern.{Backoff, BackoffSupervisor, ask}
-import my.akka.Parent.GetChildRef
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class Parent extends Actor {
-  println(s"[Parent]  ${System.currentTimeMillis()}: created.         (path = ${this.self.path},         instance = ${this})")
 
-  var children: Map[Int, ActorRef] = Map()
-
-  for(i <- 1 to 2)
-    children = children.updated(i, context.actorOf(Child.props(this.toString, i), "child-" + i))
-
-  def receive = {
-    case "boom" => throw new Exception("kaboom")
-    case GetChildRef(i) =>
-      val childRef = children(i)
-      sender() ! childRef
-  }
+class Child extends Actor {
+  println(s"[Child]: created.         (path = ${this.self.path}, instance = ${this})")
 
   override def preStart(): Unit = {
-    println(s"[Parent]  ${System.currentTimeMillis()}: preStart called. (path = ${this.self.path},         instance = ${this})")
+    println(s"[Child]: preStart called. (path = ${this.self.path}, instance = ${this})")
     super.preStart()
   }
 
   override def postStop(): Unit = {
-    println(s"[Parent]  ${System.currentTimeMillis()}: postStop called. (path = ${this.self.path},         instance = ${this})")
+    println(s"[Child]: postStop called. (path = ${this.self.path}, instance = ${this})")
     super.postStop()
   }
 
   override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
-    println(s"[Parent]  ${System.currentTimeMillis()}: preRestart called with (reason = $reason, message = $message). (path = ${this.self.path}, instance = ${this})")
+    println(s"[Child]: preRestart called with ($reason, $message). (path = ${this.self.path}, instance = ${this})")
     super.preRestart(reason, message)
   }
 
   override def postRestart(reason: Throwable): Unit = {
-    println(s"[Parent]  ${System.currentTimeMillis()}: postRestart called with (reason = $reason). (path = ${this.self.path}, instance = ${this})")
+    println(s"[Child]: postRestart called with ($reason). (path = ${this.self.path}, instance = ${this})")
     super.postRestart(reason)
   }
-}
-
-object Parent {
-  case class GetChildRef(id: Int)
-}
-
-class Child(parent: String, id: Int) extends Actor {
-  println(s"[Child($id)]${System.currentTimeMillis()}: created.         (path = ${this.self.path}, instance = ${this}, parent = ${parent})")
 
   def receive = {
+    case "boom" =>
+      throw new Exception("kaboom")
+    case "get ref" =>
+      sender() ! self
     case a: Any =>
-      println(s"[Child($id)]${System.currentTimeMillis()}: received ${a}")
-  }
-
-  override def preStart(): Unit = {
-    println(s"[Child($id)]${System.currentTimeMillis()}: preStart called. (path = ${this.self.path}, instance = ${this}, parent = ${parent})")
-    super.preStart()
-  }
-
-  override def postStop(): Unit = {
-    println(s"[Child($id)]${System.currentTimeMillis()}: postStop called. (path = ${this.self.path}, instance = ${this}), parent = ${parent}")
-    super.postStop()
-  }
-
-  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
-    println(s"[Child($id)]${System.currentTimeMillis()}: preRestart called with ($reason, $message). (path = ${this.self.path}, instance = ${this}), parent = ${parent}")
-    super.preRestart(reason, message)
-  }
-
-  override def postRestart(reason: Throwable): Unit = {
-    println(s"[Child($id)]${System.currentTimeMillis()}: postRestart called with ($reason). (path = ${this.self.path}, instance = ${this}), parent = ${parent}")
-    super.postRestart(reason)
+      println(s"[Child]: received ${a}")
   }
 }
 
 object Child {
-  def props(parentInstanceName: String, id: Int): Props
-    = Props(new Child(parentInstanceName, id))
+  def props: Props
+    = Props(new Child)
 
-  def backOffProps(parentInstanceName: String, id: Int): Props
+  def backOffOnFailureProps: Props
     = BackoffSupervisor.props(
     Backoff.onFailure(
-      Child.props(parentInstanceName, id),
+      Child.props,
       childName = "myEcho",
-      minBackoff = 3.seconds,
+      minBackoff = 1.seconds,
       maxBackoff = 30.seconds,
+      randomFactor = 0.2 // adds 20% "noise" to vary the intervals slightly
+    ))
+
+  def backOffOnStopProps: Props
+  = BackoffSupervisor.props(
+    Backoff.onStop(
+      Child.props,
+      childName = "myEcho",
+      minBackoff = 1.seconds,
+      maxBackoff = 10.seconds,
       randomFactor = 0.2 // adds 20% "noise" to vary the intervals slightly
     ))
 }
 
 object BackoffSuperVisorApp {
-  def main(args: Array[String]): Unit ={
+  def defaultSuperVisorCase(): Unit = {
+    println(
+      """
+        |default ---------------------------
+      """.stripMargin)
+
     val system = ActorSystem("app")
     try{
-      implicit val futureTimeout: akka.util.Timeout = 1.second
-      val parent = system.actorOf(Props[Parent], "parent")
+      /**
+       * Let's see if "hello" message is received by the child
+       */
+      val child = system.actorOf(Child.props, "child")
       Thread.sleep(100)
-      val child1 = Await.result(parent ? GetChildRef(1), 1.second).asInstanceOf[ActorRef]
-      child1 ! "hello"
-      //[Child(1)]1505661586660: received hello
-      parent ! "boom"
+      child ! "hello"
+      //[Child]: received hello
+
+      /**
+       * Now restart the child with an exception within its receive method
+       * and see if the `child` ActorRef is still valid (i.e. ActorRef incarnation remains same)
+       */
+      child ! "boom"
       Thread.sleep(200)
 
-      child1 ! "hello"
+      child ! "hello after normal exception"
+      //[Child]: received hello after normal exception
+
       /**
-       * This causes delivery to deadLetter, since the "incarnation" of ActorRef child1 is updated
-       * after the parent stopped child upon "kaboom" exception
+       * PoisonPill causes the child actor to `Stop`, different from restart.
+       * The ActorRef incarnation gets updated.
+       */
+      child ! PoisonPill
+      Thread.sleep(200)
+
+      /**
+       * This causes delivery to deadLetter, since the "incarnation" of ActorRef `child` became obsolete
+       * after child is "Stopped"
        *
-       *   [akka://app/user/parent/child-1] Message [java.lang.String] without sender to Actor[akka://app/user/parent/child-1#-767539042]
-       *   was not delivered. [1] dead letters encountered.
-       *
-       * An incarnation is tied to an ActorRef, not its internal actor instance,
+       * An incarnation is tied to an ActorRef (NOT to its internal actor instance)
        * and the same incarnation means "you can keep using the same ActorRef"
        */
-      parent ! "boom"
-      parent ! PoisonPill
+      child ! "hello after PoisonPill"
+      // [akka://app/user/parent/child-1] Message [java.lang.String] without sender to Actor[akka://app/user/child#-767539042]
+      //   was not delivered. [1] dead letters encountered.
+
       Thread.sleep(200)
-      parent ! "boom"
     }
     finally{
       system.terminate()
+      Thread.sleep(500)
     }
+  }
+
+  def backOffOnStopCase(): Unit ={
+    println(
+      """
+        |backoff onStop ---------------------------
+      """.stripMargin)
+
+    val system = ActorSystem("app")
+    try{
+      /**
+       * Let's see if "hello" message is forwarded to the child
+       * by the backoff supervisor onStop
+       */
+      implicit val futureTimeout: akka.util.Timeout = 1.second
+      val backoffSupervisorActor = system.actorOf(Child.backOffOnStopProps, "child")
+      Thread.sleep(100)
+
+      backoffSupervisorActor ! "hello to backoff supervisor" //forwarded to child
+      //[Child]: received hello to backoff supervisor
+
+      /**
+       * Now "Restart" the child with an exception from its receive method.
+       * As with the default supervisory strategy, the `child` ActorRef remains valid. (i.e. incarnation kept same)
+       */
+      val child = Await.result(backoffSupervisorActor ? "get ref", 1.second).asInstanceOf[ActorRef]
+      child ! "boom"
+      Thread.sleep(2000)
+
+      child ! "hello to child after normal exception"
+      //[Child]: received hello to child after normal exception
+
+      /**
+       * Backoff Supervisor can still forward the message
+       */
+      backoffSupervisorActor ! "hello to backoffSupervisorActor after normal exception"
+      //[Child]: received hello to backoffSupervisorActor after normal exception
+
+      Thread.sleep(200)
+
+      /**
+       * PoisonPill causes the child actor to `Stop`, different from restart.
+       * The `child` ActorRef incarnation gets updated.
+       */
+      child ! PoisonPill
+      Thread.sleep(2000)
+
+      child ! "hello to child ref after PoisonPill"
+      //delivered to deadLetters
+
+      /**
+       * Backoff Supervisor can forward the message to its child with the new incarnation
+       */
+      backoffSupervisorActor ! "hello to backoffSupervisorActor after PoisonPill"
+      //[Child]: received hello to backoffSupervisorActor after PoisonPill
+
+      Thread.sleep(200)
+    }
+    finally{
+      system.terminate()
+      Thread.sleep(500)
+    }
+  }
+
+  def backOffOnFailureCase(): Unit ={
+    println(
+      """
+        |backoff onFailure ---------------------------
+      """.stripMargin)
+
+    val system = ActorSystem("app")
+    try{
+      /**
+       * Let's see if "hello" message is forwarded to the child
+       * by the backoff supervisor onFailure
+       */
+      implicit val futureTimeout: akka.util.Timeout = 1.second
+      val backoffSupervisorActor = system.actorOf(Child.backOffOnFailureProps, "child")
+      Thread.sleep(100)
+
+      backoffSupervisorActor ! "hello to backoff supervisor" //forwarded to child
+      //[Child]: received hello to backoff supervisor
+
+      /**
+       * Now "Stop" the child with an exception from its receive method.
+       * You'll see the difference between "Restart" and "Stop" from here:
+       */
+      val child = Await.result(backoffSupervisorActor ? "get ref", 1.second).asInstanceOf[ActorRef]
+      child ! "boom"
+      Thread.sleep(2000)
+
+      /**
+       * Note that this is after normal exception, not after PoisonPill,
+       * but child is completely "Stopped" and its ActorRef "incarnation" became obsolete
+       *
+       * So, the message to the `child` ActorRef is delivered to deadLetters
+       */
+      child ! "hello to child after normal exception"
+      //causes delivery to deadLetter
+
+      /**
+       * Backoff Supervisor can still forward the message to the new child ActorRef incarnation
+       */
+      backoffSupervisorActor ! "hello to backoffSupervisorActor after normal exception"
+      //[Child]: received hello to backoffSupervisorActor after normal exception
+
+      /**
+       * You can get a new ActorRef which represents the new incarnation
+       */
+      val newChildRef = Await.result(backoffSupervisorActor ? "get ref", 1.second).asInstanceOf[ActorRef]
+      newChildRef ! "hello to new child ref after normal exception"
+      //[Child]: received hello to new child ref after normal exception
+
+      Thread.sleep(200)
+
+      /**
+       * No matter whether the supervisory strategy is default or backoff,
+       * PoisonPill causes the actor to "Stop", not "Restart"
+       */
+      newChildRef ! PoisonPill
+      Thread.sleep(3000)
+
+      newChildRef ! "hello to new child ref after PoisonPill"
+      //delivered to deadLetters
+
+      Thread.sleep(200)
+    }
+    finally{
+      system.terminate()
+      Thread.sleep(500)
+    }
+  }
+
+  def main(args: Array[String]): Unit ={
+    defaultSuperVisorCase()
+    backOffOnStopCase()
+    backOffOnFailureCase()
   }
 }
