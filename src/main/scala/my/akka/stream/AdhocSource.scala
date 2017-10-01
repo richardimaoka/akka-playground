ad81a2fa9de127b44b1b94dd09be30116cd1f439
Richard Imaoka <richard.e.imaoka@gmail.com>
2017-10-01 22:00:00 +0900

    Adhoc Source changes

--- src/main/scala/my/akka/stream/AdhocSource.scala
+++ src/main/scala/my/akka/stream/AdhocSource.scala
@@ -9,11 +9,30 @@ import scala.concurrent.duration.FiniteDuration
 
 object AdhocSource {
 
-  def adhocSource[T](timeout: FiniteDuration, maxRetries: Int, source: Source[T, _]): Source[T, _] =
+  def adhocSource[T](timeout: FiniteDuration, maxRetries: Int, source: Source[T, _], printCount: Boolean = false): Source[T, _] =
+    Source
+      .lazily(
+        () => source
+          .backpressureTimeout(timeout)
+          .recoverWithRetries(maxRetries, {
+            case t: TimeoutException =>
+              if(printCount)
+                println(s"creating lazy source with remaining count = ${maxRetries - 1}")
+              adhocSource(timeout, maxRetries - 1, source).asInstanceOf[Graph[SourceShape[T], NotUsed]]
+          })
+      )
+
+  /**
+   * This is wrong!
+   * Once the lazy source started, backpressureTimeout keeps throwing TimeoutException
+   * and recoverWithRetries is kept being called
+   */
+  def adhocSource2[T](timeout: FiniteDuration, maxRetries: Int, source: Source[T, _], printCount: Boolean = false): Source[T, _] =
     Source
       .lazily(() => source)
       .backpressureTimeout(timeout).recoverWithRetries(maxRetries, {
         case t: TimeoutException =>
+          println(s"creating lazy source with remaining count = ${maxRetries - 1}")
           adhocSource(timeout, maxRetries - 1, source).asInstanceOf[Graph[SourceShape[T], NotUsed]]
       })
 
--- src/test/scala/my/akka/stream/AdhocSourceSpec.scala
+++ src/test/scala/my/akka/stream/AdhocSourceSpec.scala
@@ -26,22 +26,86 @@ class AdhocSourceSpec extends TestKit(ActorSystem("AdhocSourceSpec")) with WordS
       val timeout = 1000.milliseconds
     }
 
-    "time out after the preset timeout value has passed" in {
-      val source = Source.repeat(5)
+    "not start the source even after run, when there is no demand" in {
+      val materialized = new AtomicBoolean()
       val timeout = 200.milliseconds
+      val source = Source.empty.mapMaterializedValue(_ => materialized.set(true)) // a Source which sets AtomicBoolean = true on start
 
-      val timeoutSource = AdhocSource.timeoutSource(timeout, source)
-      val mat = timeoutSource.runWith(TestSink.probe[Int])
-      mat.request(1).expectNext(5)
-      mat.request(1).expectNext(5)
-      mat.request(1).expectNext(5)
+      val mat = AdhocSource.adhocSource2(timeout, 3, source, true)
+        .runWith(TestSink.probe[Int])
       Thread.sleep(500)
-      mat.request(1).expectError().getClass should be(classOf[TimeoutException])
+
+      materialized.get() should be(false)
+    }
+
+    "materialize after run AND when there is a demand" in {
+      val materialized = new AtomicBoolean()
+      val timeout = 200.milliseconds
+      val mat = AdhocSource.adhocSource(timeout, 3, Source.empty.mapMaterializedValue(_ => materialized.set(true)))
+        .runWith(TestSink.probe[Int])
+      mat.request(1)
+      Thread.sleep(500)
+      materialized.get() should be(true)
+    }
+
+    "shut down the source when a single consumer disconnects" in {
+      val shutdown = Promise[Done]()
+      val timeout = 200.milliseconds
+      val mat = AdhocSource.adhocSource(
+        timeout,
+        3,
+        Source.repeat(5).watchTermination() { (_, term) =>
+          shutdown.completeWith(term)
+        }).runWith(Sink.head[Int])
+
+      Thread.sleep(500)
+
+      /**
+       * Sink.head terminates when receiving the first element.
+       * If you use Sink.ignore, then the future times out:
+       *   java.util.concurrent.TimeoutException: Futures timed out after [10 seconds
+       */
+      Await.ready(shutdown.future, 10.seconds)
+    }
+
+    "shut down the source when there is no demand until timeout" in {
+      val timeout = 1000.milliseconds
+      val materialized = new AtomicBoolean()
+
+      val source1 = Source.empty.mapMaterializedValue(_ => materialized.set(true))
+      val source2 = Source.repeat(5)
+
+      val (watchTerm, sink) = AdhocSource.timeoutSource(
+        timeout,
+        source1.concat(source2)
+      ).watchTermination()(Keep.right)
+        .toMat(TestSink.probe[Int])(Keep.both)
+        .run()
+
+      Thread.sleep(300)
+      materialized.get() should be(false)
+
+      sink.requestNext(5)
+      sink.requestNext(5)
+      sink.requestNext(5)
+      Thread.sleep(3000)
+
+      sink.expectError().getClass should be(classOf[TimeoutException])
+      import scala.concurrent.ExecutionContext.Implicits.global
+      watchTerm onComplete{
+        case Success(a) => fail("This future should not succeed!")
+        case Failure(a) => succeed
+      }
     }
 
     "wait in lazy source after timeout" in {
       val timeout = 200.milliseconds
       val materialized = new AtomicInteger(0)
+
+      /**
+       * Once materialized, this source immediately increment the materialized AtomicInteger,
+       * and switch the source to Source.repeat(5) <- the first element is 5 from this Source.repeat
+       */
       val source = Source
         .empty.mapMaterializedValue(_ => materialized.incrementAndGet())
         .concat(Source.repeat(5))
@@ -56,31 +120,30 @@ class AdhocSourceSpec extends TestKit(ActorSystem("AdhocSourceSpec")) with WordS
       // Source not materialized yet
       materialized.get() should be(0)
 
-      sink.request(1).expectNext(5)
       // Source materialized after the first demand
-      materialized.get() should be(1)
-      sink.request(1).expectNext(5)
       sink.request(1).expectNext(5)
       materialized.get() should be(1)
 
-      Thread.sleep(500)
+      // After few requests the incremental count is still = 1
+      sink.request(1).expectNext(5)
+      sink.request(1).expectNext(5)
+      materialized.get() should be(1)
 
       //After timeout has passed, there must be TimeoutException
+      Thread.sleep(500)
       import scala.concurrent.ExecutionContext.Implicits.global
       watchTerm onComplete{
         case Success(a) => fail("This future should not succeed!")
-        case Failure(a) => succeed
+        case Failure(a) => succeed //confirmed this still keeps the succeeding part of the test running
       }
-
       materialized.get() should be(1)
 
       //on the next demand, the source should be materialized again
       sink.request(1).expectNext(5)
       materialized.get() should be(2)
 
+      //Terminated, then if no new demand, the source is still lazy i.e. not materialized
       Thread.sleep(500)
-
-      //if no new demand, the source is still lazy i.e. not materialized
       materialized.get() should be(2)
 
     }
@@ -129,6 +192,20 @@ class AdhocSourceSpec extends TestKit(ActorSystem("AdhocSourceSpec")) with WordS
       materialized.get() should be(true)
     }
 
+
+    "time out after the preset timeout value has passed" in {
+      val source = Source.repeat(5)
+      val timeout = 200.milliseconds
+
+      val timeoutSource = AdhocSource.timeoutSource(timeout, source)
+      val mat = timeoutSource.runWith(TestSink.probe[Int])
+      mat.request(1).expectNext(5)
+      mat.request(1).expectNext(5)
+      mat.request(1).expectNext(5)
+      Thread.sleep(500)
+      mat.request(1).expectError().getClass should be(classOf[TimeoutException])
+    }
+
     "shut down the source when a single consumer disconnects" in {
       val shutdown = Promise[Done]()
       val timeout = 200.milliseconds
@@ -174,6 +251,5 @@ class AdhocSourceSpec extends TestKit(ActorSystem("AdhocSourceSpec")) with WordS
         case Failure(a) => succeed
       }
     }
-
   }
 }
