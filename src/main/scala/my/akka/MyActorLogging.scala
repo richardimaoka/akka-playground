c86ca765bf442b41372bdfae132dd85ac7db9a64
Richard Imaoka <richard.e.imaoka@gmail.com>
2017-11-09 07:50:24 +0900

    WIP

--- src/main/scala/my/akka/MyActorLogging.scala
+++ src/main/scala/my/akka/MyActorLogging.scala
@@ -1,7 +1,8 @@
 package my.akka
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorSelectionMessage, ActorSystem, Props}
 import akka.event.Logging
+import my.akka.wrapper.Wrap
 
 class MyActor extends Actor {
   val log = Logging(context.system, this)
@@ -33,19 +34,62 @@ class MyActorMixedIn extends Actor with ActorLogging {
 }
 
 object MyActorLogging {
-  def main(args: Array[String]): Unit = {
-    val system = ActorSystem("MyActorLogging")
-    try {
-      val actor1 = system.actorOf(Props(new MyActor), "actor1")
-      val actor2 = system.actorOf(Props(new MyActor), "actor2")
-      actor1 ! "test"
-      //[INFO] [10/31/2017 05:32:24.279] [MyActorLogging-akka.actor.default-dispatcher-3] [akka://MyActorLogging/user/actor1] Received test
-      actor2 ! "test"
-      //[INFO] [10/31/2017 05:32:24.279] [MyActorLogging-akka.actor.default-dispatcher-4] [akka://MyActorLogging/user/actor2] Received test
+  val system = ActorSystem("MyActorLogging")
 
-      val args = Array("The", "brown", "fox", "jumps", 42)
-      system.log.debug("five parameters: {}, {}, {}, {}, {}", args)
-      
+  def basics(): Unit ={
+    val actor1 = system.actorOf(Props(new MyActor), "actor1")
+    val actor2 = system.actorOf(Props(new MyActor), "actor2")
+    actor1 ! "test"
+    //[INFO] [10/31/2017 05:32:24.279] [MyActorLogging-akka.actor.default-dispatcher-3] [akka://MyActorLogging/user/actor1] Received test
+    actor2 ! "test"
+    //[INFO] [10/31/2017 05:32:24.279] [MyActorLogging-akka.actor.default-dispatcher-4] [akka://MyActorLogging/user/actor2] Received test
+
+    val args = Array("The", "brown", "fox", "jumps", 42)
+    system.log.info("five parameters: {}, {}, {}, {}, {}", args)
+    //[INFO] [11/02/2017 06:57:17.831] [run-main-1] [akka.actor.ActorSystemImpl(MyActorLogging)] five parameters: The, brown, fox, jumps, 42
+
+    system.log.debug("five parameters: {}, {}, {}, {}, {}", args)
+    //prints out nothing as the default debug level is not debug
+    println(system.log.isDebugEnabled)
+  }
+
+  def printName(): Unit ={
+    println(Logging.simpleName(this))
+    //MyActorLogging$
+
+    println(Logging.messageClassName("some message"))
+    //java.lang.String
+  }
+
+  def logLevel(): Unit ={
+    println(Logging.LogLevel(0))
+    println(Logging.LogLevel(1))
+    println(Logging.LogLevel(2))
+    println(Logging.LogLevel(3))
+    println(Logging.LogLevel(4))
+    println(Logging.LogLevel(5))
+    println(Logging.LogLevel(6))
+    // LogLevel(0)
+    // LogLevel(1)
+    // LogLevel(2)
+    // LogLevel(3)
+    // LogLevel(4)
+    // LogLevel(5)
+    // LogLevel(6)
+    println(Logging.ErrorLevel)   // LogLevel(1)
+    println(Logging.WarningLevel) // LogLevel(2)
+    println(Logging.InfoLevel)    // LogLevel(3)
+    println(Logging.DebugLevel)   // LogLevel(4)
+
+    // oops this is not possible, as `OffLevel` is private
+    // println(Logging.OffLevel)
+  }
+
+  def main(args: Array[String]): Unit = {
+    try {
+      Wrap("basics")(basics())
+      Wrap("printName")(printName())
+      Wrap("logLevel")(logLevel())
     } finally {
       system.terminate()
     }
---
+++ src/main/scala/my/akka/wrapper/Wrapper.scala
@@ -0,0 +1,13 @@
+package my.akka.wrapper
+
+object Wrap {
+  def apply(callerFunctionName: String = "f")(unitFunction: => Unit): Unit = {
+    val header = "----" + callerFunctionName + "------------------------------------------------------------------"
+    println(header.substring(0, 60))
+
+    unitFunction // call the passed function
+
+    Thread.sleep(50)
+    println()
+  }
+}
