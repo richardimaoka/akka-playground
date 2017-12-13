9207dacbe4f1da47e7377736a4470533f984fd64
Richard Imaoka <richard.e.imaoka@gmail.com>
2017-12-14 00:43:58 +0900

    Tweaking and correction, especially in terms of manifest

--- src/main/scala/my/akka/typed/MySerializer.scala
+++ src/main/scala/my/akka/typed/MySerializer.scala
@@ -2,8 +2,8 @@ package my.akka.typed
 
 import java.io.{PrintWriter, StringWriter}
 
-import akka.serialization.SerializationExtension
-import com.typesafe.config.ConfigFactory
+import akka.serialization.{SerializationExtension, SerializerWithStringManifest}
+import com.typesafe.config.{Config, ConfigFactory}
 import my.akka.wrapper.Wrap
 
 class EmptyActor extends akka.actor.Actor {
@@ -83,45 +83,43 @@ object MySerializer {
     }
   }
 
-  def serializerForTypedActorRef(): Unit ={
+  def serializeTypedActorRef(config: Config): Unit = {
     import akka.typed.scaladsl._
     import akka.typed.scaladsl.adapter._
 
-    val greeter = Actor.immutable[String] { (_, msg) ⇒
-      println(s"typed actor received a message ${msg}!")
+    val empty = Actor.immutable[String] { (_, msg) ⇒
       akka.typed.scaladsl.Actor.same
     }
 
-    val config = ConfigFactory.parseString(
-      """akka {
-        |  actor {
-        |    serialization-bindings {
-        |      "akka.typed.ActorRef" = java
-        |      "akka.typed.internal.adapter.ActorRefAdapter" = java
-        |    }
-        |  }
-        |}
-      """.stripMargin)
-
     val system = akka.actor.ActorSystem("example", config)
     try {
-      val original = system.spawn(greeter, "greeter")
+      val original = system.spawn(empty, "empty")
       val serialization = SerializationExtension(system)
+
       val serializer = serialization.findSerializerFor(original)
 
+      //Serializer for class akka.typed.internal.adapter.ActorRefAdapter = akka.serialization.JavaSerializer@7389af41
       println(s"Serializer for ${original.getClass} = " + serializer)
-      /**
-        * As in https://github.com/akka/akka/pull/23696/files
-        * Serializer for class akka.typed.internal.adapter.ActorRefAdapter = akka.typed.cluster.internal.MiscMessageSerializer@37f16b1b
-        */
 
       // Turn it into bytes
       val bytes = serializer.toBinary(original)
+
       // Turn it back into an object
-      val back = serializer.fromBinary(bytes, manifest = None)
+      val back = serialization.findSerializerFor(original) match {
+        case s: SerializerWithStringManifest ⇒ s.fromBinary(bytes, s.manifest(original))
+        case _ => serializer.fromBinary(bytes, manifest = None)
+      }
+
       // Voilá!
       println(back, " ", back.getClass)
       println(original, " ", original.getClass)
+    } catch {
+      case t: Throwable => {
+        val sw = new StringWriter
+        t.printStackTrace(new PrintWriter(sw))
+        println(t.getMessage)
+        println(sw)
+      }
     } finally {
       system.terminate()
     }
@@ -146,6 +144,13 @@ object MySerializer {
       val bindingPath = "akka.actor.serialization-bindings"
       println(bindingPath)
       println(system.settings.config.getConfig(bindingPath).root.render)
+    } catch {
+      case t: Throwable => {
+        val sw = new StringWriter
+        t.printStackTrace(new PrintWriter(sw))
+        println(t.getMessage)
+        println(sw)
+      }
     } finally {
       system.terminate()
     }
@@ -156,6 +161,63 @@ object MySerializer {
     Wrap("displayConfig")(displayConfig)
     Wrap("sampleFromAkkaDoc")(sampleFromAkkaDoc)
     Wrap("serializerForUntypedActorRef")(serializerForUntypedActorRef)
-    Wrap("serializerForTypedActorRef")(serializerForTypedActorRef)
+
+    Wrap("typedWithJavaSerializer")(serializeTypedActorRef(
+      ConfigFactory.parseString(
+        """akka {
+          |  actor {
+          |    serialization-bindings {
+          |      "akka.typed.ActorRef" = java
+          |      "akka.typed.internal.adapter.ActorRefAdapter" = java
+          |    }
+          |  }
+          |}
+        """.stripMargin)
+    ))
+    /**
+      *  This uses a config to override the serializers for typed ActorRef and ActorRefAdapter
+      *  to use the Java Serializer ...
+      *
+      *  ... and gives the following exception since akka.typed.internal.adapter.ActorRefAdapter
+      *  doesn't extend Java's Serializable interface
+      *
+      *  java.io.NotSerializableException: akka.typed.internal.adapter.ActorRefAdapter
+      *        at java.io.ObjectOutputStream.writeObject0(ObjectOutputStream.java:1184)
+      *        at java.io.ObjectOutputStream.writeObject(ObjectOutputStream.java:348)
+      *        at akka.serialization.JavaSerializer.$anonfun$toBinary$1(Serializer.scala:319)
+      *        at scala.runtime.java8.JFunction0$mcV$sp.apply(JFunction0$mcV$sp.java:12)
+      *        at scala.util.DynamicVariable.withValue(DynamicVariable.scala:58)
+      *        at akka.serialization.JavaSerializer.toBinary(Serializer.scala:319)
+      *        at my.akka.typed.MySerializer$.main(MySerializer.scala:36)
+      *        at my.akka.typed.MySerializer.main(MySerializer.scala)
+      *        at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+      *        at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+      *        at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+      */
+
+    Wrap("typedWithMiscSerializer")(serializeTypedActorRef(ConfigFactory.parseString("")))
+    /**
+      *  This will use the default config as follows:
+      *    # https://github.com/akka/akka/pull/23696/files
+      *    # akka-typed/src/main/resources/reference.conf
+      *    akka.actor {
+      *      serializers {
+      *        typed-misc = "akka.typed.cluster.internal.MiscMessageSerializer"
+      *      }
+      *      serialization-identifiers {
+      *        "akka.typed.cluster.internal.MiscMessageSerializer" = 24
+      *      }
+      *      serialization-bindings {
+      *        "akka.typed.ActorRef" = typed-misc
+      *        "akka.typed.internal.adapter.ActorRefAdapter" = typed-misc
+      *      }
+      *    }
+      *
+      *  and produces the following output (i.e. successful serialization/deserialization) :
+      *
+      *    Serializer for class akka.typed.internal.adapter.ActorRefAdapter = akka.typed.cluster.internal.MiscMessageSerializer@387cd6cd
+      *    (Actor[akka://example/user/empty#-205098054], ,class akka.typed.internal.adapter.ActorRefAdapter)
+      *    (Actor[akka://example/user/empty#-205098054], ,class akka.typed.internal.adapter.ActorRefAdapter)
+      */
   }
 }
